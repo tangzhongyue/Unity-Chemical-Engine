00020ee099e4326280868ac5f53f1449f1f05bf8
vitalight <904844050@qq.com>
2018-05-19 21:31:47 +0800

    update dropper and README.md

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,24 @@
+﻿namespace UCE
+{
+    using UnityEngine;
+    using VRTK;
+
+    public class Dropper : VRTK_InteractableObject
+    {
+        private bool hasWater = true;
+
+        public override void StartUsing(VRTK_InteractUse usingObject)
+        {
+            base.StartUsing(usingObject);
+            Debug.Log("StartUsing");
+            transform.Find("water").gameObject.SetActive(!hasWater);
+            hasWater = !hasWater;
+        }
+
+        public override void StopUsing(VRTK_InteractUse usingObject)
+        {
+            base.StopUsing(usingObject);
+            Debug.Log("StopUsing");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Unity Chemical Engine
 
 SJTU Game Programming final
+
+
+
+##基本的运行逻辑
+
+* VR模拟器操作，用手柄碰到可交互物体时周围有黄线提示
+* 左键可以拿起来，再按左键放下
+* 把铜块放进烧杯之后，会触发UCE_Chemical.cs中的OnTriggerEnter脚本，调用UCE_Engine输入反应物质，获取反应效果，然后调用UCE_Animation显示动画（现在是铜块消失，烧杯里水减半）
+* 之后化学反应引擎部分修改UCE_Engine.cs，动画部分修改UCE_Animation就差不多了
+
+
+
+* 另外一方面滴管正在做，截至目前拿起来之后按右键触发Dropper.cs中脚本，切换有水/无水状态，之后改来从试管里吸取液体
+
