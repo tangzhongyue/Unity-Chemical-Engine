ccd38ee124c5624018d0d1db2b64c0f00e4513c6
vitalight <904844050@qq.com>
2018-05-23 22:26:45 +0800

    尝试在接触时显示化学物质名称[TODO]

---
+++ Assets/Scripts/ShowNameOnTouch.cs
@@ -0,0 +1,43 @@
+﻿namespace UCE
+{
+    using UnityEngine;
+    using VRTK;
+
+    public class ShowNameOnTouch : MonoBehaviour {
+
+        public string showName;
+        public Vector3 offset;
+        public GameObject textObject;
+
+        private bool show = false;
+
+	    void OnTriggerEnter(Collider other)
+        {
+            //if (other.GetComponent<VRTK_ControllerEvents>())
+            {
+                show = true;
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            //if (other.GetComponent<VRTK_ControllerEvents>())
+            {
+                show = false;
+            }
+        }
+
+        void OnGUI()
+        {
+            //if (show)
+            {
+                textObject.SetActive(true);
+                textObject.transform.position = transform.position + offset;
+                textObject.GetComponent<TextMesh>().text = showName;
+                textObject.transform.LookAt(Camera.main.transform.position, Vector3.up);
+                textObject.transform.forward = Camera.main.transform.forward;
+            }
+        }
+
+    }
+}
