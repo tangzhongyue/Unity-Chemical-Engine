89954bc2cdd2933403abaf076b698f8202696142
tangzhongyue <853752063@qq.com>
2018-05-31 13:58:49 +0800

    delete useless reactsys✧(≖ ◡ ≖✿)

--- Assets/Scripts/Animation/Bubbles.cs
+++ Assets/Scripts/Animation/Bubbles.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReactionPhenomena : MonoBehaviour {
-	
+public class Bubbles : MonoBehaviour {
+	public GameObject bubbles;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
+		
+	}
+
+	public void CreateBubbles(float r) {
 		
 	}
 }
