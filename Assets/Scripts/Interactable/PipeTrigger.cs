0a0f966df26ead3452df7060a12aa9a400f8333f
vitalight <904844050@qq.com>
2018-06-14 20:44:03 +0800

    完成集气瓶（导管深入瓶中时触发集气动画，集气瓶可以正放在桌上紫色区域）

---
+++ Assets/Scripts/Interactable/PipeTrigger.cs
@@ -0,0 +1,34 @@
+﻿namespace UCE
+{
+    using UnityEngine;
+
+    public class PipeTrigger : MonoBehaviour {
+
+        public GameObject bottle;
+
+        private Bottle bot;
+
+        void Start()
+        {
+            bot = bottle.GetComponent<Bottle>();
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.name == "bottle")
+            {
+                bot.pipeIn = true;
+                Debug.Log("PipeTrigger: in Bottle");
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.name == "bottle")
+            {
+                bot.pipeIn = false;
+                Debug.Log("PipeTrigger: out Bottle");
+            }
+        }
+    }
+}
