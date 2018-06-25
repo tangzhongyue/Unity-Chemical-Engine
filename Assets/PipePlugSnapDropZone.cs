abc7482ba744ae9ffecfa53f3f80fd93cdb42f6d
vitalight <904844050@qq.com>
2018-06-25 13:09:59 +0800

    Add super Fe & Add air transmit module

---
+++ Assets/PipePlugSnapDropZone.cs
@@ -0,0 +1,25 @@
+﻿namespace UCE
+{
+    using UnityEngine;
+    using VRTK;
+
+    public class PipePlugSnapDropZone : VRTK_SnapDropZone
+    {
+
+        public override void OnObjectSnappedToDropZone(SnapDropZoneEventArgs e)
+        {
+            base.OnObjectSnappedToDropZone(e);
+            Debug.Log(e.snappedObject.name + " in");
+            AirTransmit air = transform.GetComponent<AirTransmit>();
+            AirTransmit.Connect(air, e.snappedObject.GetComponent<AirTransmit>());
+        }
+
+        public override void OnObjectUnsnappedFromDropZone(SnapDropZoneEventArgs e)
+        {
+            base.OnObjectUnsnappedFromDropZone(e);
+            Debug.Log(e.snappedObject.name + " out");
+            AirTransmit air = transform.GetComponent<AirTransmit>();
+            AirTransmit.Disconnect(air, e.snappedObject.GetComponent<AirTransmit>());
+        }
+    }
+}
