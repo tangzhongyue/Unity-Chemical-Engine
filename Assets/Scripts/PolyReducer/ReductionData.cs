ec0dcc41906a4dadc0f5ae6abfa3e7bf782bc1e9
Mr.Melon <nmgzhangran@163.com>
2018-06-26 09:50:48 +0800

    add colapse code of model

---
+++ Assets/Scripts/PolyReducer/ReductionData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+namespace PolyReduction
+{
+    public class ReductionData : ScriptableObject
+    {
+        public int vertexU;
+        public int vertexV;
+        public List<int> triangleID = new List<int>();
+    }
+}
